8f1f387756802125084e2e8753ec5552996f83f6
ejabu <muhammadfahreza@gmail.com>
2020-02-06 13:25:11 +0700

    Cafe Zip

---
+++ devel/warung_steak/cafe_zip/__manifest__.py
@@ -0,0 +1,34 @@
+{
+  'name': 'Cafe Zip',
+  'author': 'Arkana',
+  'version': '0.1',
+  'depends': [
+    'cafe',
+    'cafe_draft',
+    'cafe_draft_popup',
+    'cafe_log',
+    'cafe_payment',
+    'cafe_payment_method',
+    'cafe_print',
+    'cafe_print_popup',
+    'cafe_produk_tag',
+    'cafe_security',
+    'cafe_sql_execute',
+    'cafe_xpath',
+    'cafe_yearly_graph',
+    'cafe_yearly_pivot',
+  ],
+  'data': [
+  ],
+  'qweb': [
+  ],
+  'sequence': 100,
+  'auto_install': False,
+  'installable': True,
+  'application': True,
+  'category': '- Arkademy Part 5',
+  'summary': 'One-click Installation',
+  'license': 'OPL-1',
+  'website': 'https://www.arkana.co.id/',
+  'description': '-'
+}
