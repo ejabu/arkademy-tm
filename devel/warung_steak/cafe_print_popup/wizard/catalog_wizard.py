11e1048375313e387b0ab30a5e5416ae35bb7cda
ejabu <muhammadfahreza@gmail.com>
2020-02-06 14:16:47 +0700

    Print Popup Update

--- devel/warung_steak/cafe_print_popup/wizard/catalog_wizard.py
+++ devel/warung_steak/cafe_print_popup/wizard/catalog_wizard.py
@@ -1,6 +1,7 @@
 from odoo import fields, models, api, _
 from odoo.exceptions import UserError, ValidationError
 
+
 class CatalogWizard(models.TransientModel):
 
     _name = 'catalog.wizard'
@@ -9,12 +10,32 @@ class CatalogWizard(models.TransientModel):
         selection=[
             ('makanan', 'Makanan'),
             ('minuman', 'Minuman'),
-            ],
+        ],
         string='Catalog Type',
         required=True
-        )
+    )
 
     def process_wizard(self):
-        search_query = [('tipe', '=', self.catalog_type)]
+        search_query = [
+            ('tipe', '=', self.catalog_type),
+            ('is_aktif', '=', True),
+        ]
         produk_docs = self.env['cafe.produk'].search(search_query)
-        return self.env.ref('cafe_print_popup.cafe_produk_print').report_action(produk_docs)
+        report_xml_id = self.env.ref('cafe_print_popup.cafe_produk_print')
+        vals = report_xml_id.report_action(produk_docs)
+
+        # return vals
+        return {
+            'type': 'ir.actions.report',
+            'name': 'Cafe Produk',
+            'report_type': 'qweb-pdf',
+            'report_file': False,
+            'report_name': 'cafe_print_popup.cafe_produk_template',
+            'data': None,
+            'context': {
+                'active_ids': produk_docs.ids,
+                'lang': 'en_US',
+                'tz': 'Europe/Brussels',
+                'uid': 1,
+            },
+        }
