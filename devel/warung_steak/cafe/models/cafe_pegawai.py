e42093eab8198b256cc4c14dbbc06ff321fa095c
ejabu <muhammadfahreza@gmail.com>
2020-02-06 13:20:22 +0700

    Add Binary File Columns

--- devel/warung_steak/cafe/models/cafe_pegawai.py
+++ devel/warung_steak/cafe/models/cafe_pegawai.py
@@ -7,14 +7,20 @@ class CafePegawai(models.Model):
     alamat = fields.Text(string='Alamat')
     tahun_masuk = fields.Integer(string='Tahun Masuk')
     indeks_prestasi = fields.Float(string='Indeks Prestasi')
-    
+
     tanggal_lahir = fields.Date(string='Tanggal Lahir')
     waktu_bangun = fields.Datetime(string='Waktu Bangun Tidur')
     gender = fields.Selection([
         ('l', 'Laki-laki'),
         ('p', 'Perempuan'),
     ], string='Gender')
-    
+
     is_part_time = fields.Boolean(string='Part Time')
 
     image = fields.Binary('Image')
+
+    filename_ktp = fields.Char(string='Filename KTP')
+    file_ktp = fields.Binary('Kartu Tanda Penduduk')
+    
+    filename_kk = fields.Char(string='Filename KK')
+    file_kk = fields.Binary('Kartu Keluarga')
